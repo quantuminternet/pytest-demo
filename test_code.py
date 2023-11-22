669c49e0f8252ec4616308490ea2b6d8deb12fd9
quantuminternet <49562501+quantuminternet@users.noreply.github.com>
2023-11-22 13:59:14 +0100

    Create test_code.py

---
+++ test_code.py
@@ -0,0 +1,8 @@
+import pytest
+import code
+
+@pytest.mark.parametrize(['x', 'y', 'expected'], [(1, 1, 2), (1, 2, 3), (1, 1, 3)] 
+def test_add(x, y, expected):
+  result = code.add(x, y)
+  assert result == expected
+  
